cc8483380d0bcca60045bc40469ff2a63435cc65
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2024-01-26 00:25:16 +0530

    Laptop-Scrapping-from-Amazon-using-python-BeautifulSoup-init

---
+++ Laptops-Scrapping/laptops/modals/laptop.py
@@ -0,0 +1,7 @@
+class Laptop :
+    def __init__(self, id, name, title, description, category, mrp, sellingPrice, discount, weight, brandName, imageUrl, specifications):
+        self.id = id
+        self.name = name
+        self.title = title
+        self.description = description
+        self
