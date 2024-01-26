e6809dc1d3d1c723091d6b3c224decec24d6af37
VINAY KUMAR SINGH <mrsinghvinay563@gmail.com>
2024-01-26 16:59:20 +0530

    scrapped-data-save-to-json-ndjson-file

--- Laptops-Scrapping/laptops/gzips.py
+++ Laptops-Scrapping/laptops/gzips.py
@@ -1,14 +1,19 @@
 import json
 import gzip
 
+#Gzip class is for persisting scrapped data in ndjson.gz and Json files
 class Gzip :
+
     def __init__(self):
         pass
 
+
+    #Function to save data
     def saveToGzip(self, laptops, city) :
 
-        # Define the filename for the JSON file with city name
-        filename = f'{city}.json'
+        # Define the filename for the JSON and ndjson files with city name
+        jsonFileName = f'{city}.json'
+        gzFileName = f'{city}.ndjson.gz'
 
         # Convert Laptop objects to list dictionary
         laptop_lists = []
@@ -24,6 +29,11 @@ class Gzip :
 
             laptop_lists.append(laptop_dict)
 
+            # Save to ndjson format
+            with gzip.open(gzFileName, 'wt') as f:
+                f.write(json.dumps(laptop_dict) + '\n') 
+        
+
         # Open the JSON file in write 'w' mode and write the data
-        with open(filename, 'w') as file:
+        with open(jsonFileName, 'w') as file:
             json.dump(laptop_lists, file, indent=4)
